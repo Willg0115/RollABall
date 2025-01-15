332b026555996b6c8bc3d044d8fb330865cc4853
Willg0115 <willg0115@outlook.com>
2025-01-15 14:41:46 -0500

    added jumping from plane

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] float ballSpeed;
+    [SerializeField] private float ballSpeed;
+    [SerializeField] private float jumpForce;
 
-    public void MoveBall(Vector2 input)
+
+    public void MoveBall(Vector3 input)
     {
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+        if (input.y > 0)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Jump with impluse
+        }
+       
+        Vector3 inputXZPlane = new(input.x, 0, input.z);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed, ForceMode.Force); // move with normal force/Smooth movement
     }
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,27 +3,49 @@ using UnityEngine.Events;
 
 public class InputMonitoring : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    private bool isTouchingPlane = false; // Tracks if the sphere is touching the plane
 
-    // Update is called once per frame
+    // Detect collision with the plane
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isTouchingPlane = true;
+        }
+    }
+
+    // Detect when the sphere stops touching the plane
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Plane"))
+        {
+            isTouchingPlane = false;
+        }
+    }
+
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector += Vector2.up;        
+            inputVector += Vector3.forward;       
         }
          if (Input.GetKey(KeyCode.A))
         {
-            inputVector += Vector2.left;
+            inputVector += Vector3.left;
         }
          if (Input.GetKey(KeyCode.S))
         {
-            inputVector += Vector2.down;
+            inputVector += Vector3.back;
         }
          if (Input.GetKey(KeyCode.D))
         {
-            inputVector += Vector2.right;
+            inputVector += Vector3.right;
+        }
+       if (Input.GetKeyDown(KeyCode.Space) && isTouchingPlane)
+        {
+            inputVector += Vector3.up; 
         }
         OnMove?.Invoke(inputVector);
     }
